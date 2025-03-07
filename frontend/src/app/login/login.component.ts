6e6ee4941bba43c225c6ebb3449b38af8eb1b5d9
adamk17 <adam.kucharski17@gmail.com>
2025-03-07 09:37:52 +0100

    Added login page

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  hidePassword = true;
+  
+  loginForm = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login:', this.loginForm.value.login);
+      console.log('Password:', this.loginForm.value.password);
+    }
+  }
+  
+  loginWithGoogle() {
+    console.log('Logging in with Google');
+  }
+}
