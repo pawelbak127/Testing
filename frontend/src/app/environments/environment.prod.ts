dc37d17c6664f581f806b3597868dc84c918afbc
Pawel Bak <pawel.bak@knowit.eu>
2025-03-10 10:30:00 +0100

    Adding environment.prod.ts file

---
+++ frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCrsaZMpS1qSm_NgMMjtL9IarK6_nDzec0",
+    authDomain: "testapp-2aa92.firebaseapp.com",
+    projectId: "testapp-2aa92",
+    storageBucket: "testapp-2aa92.firebasestorage.app",
+    messagingSenderId: "805885256394",
+    appId: "1:805885256394:web:aac3534138e77ed00bca33",
+    measurementId: "G-HT31FRN6MC"
+  }
+};
