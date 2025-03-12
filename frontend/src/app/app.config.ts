322e709e99f76cf9132d7537c73eb2fde8ed9edf
Pawel Bak <pawel.bak@knowit.eu>
2025-03-12 10:36:21 +0100

    Modify import of env file

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { environment } from './environments/environment';
+import { environment } from '../environments/environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 
